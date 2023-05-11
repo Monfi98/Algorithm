c3c79b118ee7c7afb0cdc198dd1e19476e71d085
신승재 <68256612+slaveshin@users.noreply.github.com>
2023-05-11 10:52:33 +0900

    [Silver II] Title: 연속합, Time: 140 ms, Memory: 83424 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  baekjoon
+//
+//  Created by 신승재 on 2023/05/10.
+//
+
+import Foundation
+
+var n = Int(readLine()!)!
+var input = readLine()
+var arr: Array<Int> = []
+
+
+if let input = input {
+    let inputs = input.components(separatedBy: " ")
+    arr = inputs.map { str in
+        return Int(str)!
+    }
+}
+
+
+for i in 1..<n {
+    arr[i] = max(arr[i], arr[i - 1] + arr[i])
+}
+
+print(arr.max()!)
