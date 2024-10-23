19c5c0b1e1f3c4b6643fe94ab9419973869330a3
신승재 <68256612+Monfi98@users.noreply.github.com>
2024-10-23 10:36:39 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+func solution(_ phone_number:String) -> String {
+    
+    let result = String(repeating: "*", count: phone_number.count - 4) + phone_number.suffix(4)
+    
+    return result
+}
