88e74a84f52dd162f89d2de0827a3387f0fab808
신승재 <68256612+Monfi98@users.noreply.github.com>
2025-10-27 17:01:18 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.swift
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.swift
@@ -1,17 +1,19 @@
 import Foundation
 
-let word = readLine()!
-var answer = 1
+let word = readLine()!.map { String($0) }
 
+var flag = false
+if word.count == 1 {
+    flag = true
+}
 
 for i in 0..<Int(word.count / 2) {
-    let startIndex = word.index(word.startIndex, offsetBy: i)
-    let endIndex = word.index(word.endIndex, offsetBy: -i - 1)
-
-    if word[startIndex] != word[endIndex] {
-        answer = 0
+    if word[i] != word[word.count - 1 - i] {
+        flag = false
         break
+    } else {
+        if !flag { flag = true }
     }
 }
 
-print(answer)
+print("\(flag ? 1 : 0)")
